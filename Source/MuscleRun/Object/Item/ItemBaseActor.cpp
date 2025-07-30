07aaec66c9a43619031275ec1437885b3a59ca07
stynerkyc <stynerkyc1@gmail.com>
2025-07-30 10:00:15 +0900

    머지용 커밋

--- Source/MuscleRun/Object/Item/ItemBaseActor.cpp
+++ Source/MuscleRun/Object/Item/ItemBaseActor.cpp
@@ -34,10 +34,52 @@ void AItemBaseActor::BeginPlay()
 	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &AItemBaseActor::OnOverlapBegin);
 }
 
-void AItemBaseActor::Destroyed()
+void AItemBaseActor::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	Super::Destroyed();
+	// 여기서 OtherActor(AMRPlayerCharacter 의 ItemActivate 함수를 통해, 호출해주세요!
+	// 끝냈다면, 에디터 세팅에서 Player의 Collision Preset에서 새 Coliision Channel, Item을 만들고, Ovelap 만 활성화!
+	// 이 액터의 콜리전 채널을 Item으로 하고, Player의 것도 켜 주세요!
+	AMRPlayerCharacter* PlayerCharacter = Cast<AMRPlayerCharacter>(OtherActor);
+	if (PlayerCharacter && ItemType != EItemEffectTypes::None)
+	{
+		PlayPickupEffect();
+		/*UE_LOG(LogTemp, Warning, TEXT("충돌: %s(%s) 와 %s(%s)"),
+			*GetName(), *OverlappedComp->GetName(),
+			*OtherActor->GetName(), *OtherComp->GetName());*/
+		PlayerCharacter->ItemActivated(ItemType);
 
+		SetActorHiddenInGame(true);
+		SetActorEnableCollision(false);
+
+		FTimerHandle DestroyTimerHandle;
+		GetWorldTimerManager().SetTimer(DestroyTimerHandle, this, &AItemBaseActor::DestroySelf, 0.8f, false);
+	}
+}
+
+void AItemBaseActor::DestroySelf()
+{
+	PlayVanishEffect();
+
+	const float VanishDelay = 2.0f;
+
+	Destroy();
+}
+
+void AItemBaseActor::PlayPickupEffect()
+{
+	if (PickupSoundCue)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, PickupSoundCue, GetActorLocation());
+	}
+	
+	if (PickupEffect)
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), PickupEffect, GetActorLocation());
+	}
+}
+
+void AItemBaseActor::PlayVanishEffect()
+{
 	if (VanishSound)
 	{
 		UGameplayStatics::PlaySoundAtLocation(this, VanishSound, GetActorLocation());
@@ -47,42 +89,5 @@ void AItemBaseActor::Destroyed()
 	{
 		UGameplayStatics::SpawnEmitterAtLocation(this, VanishEffect, GetActorLocation());
 	}
-
-	Destroy();
-}
-
-void AItemBaseActor::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	// 여기서 OtherActor(AMRPlayerCharacter 의 ItemActivate 함수를 통해, 호출해주세요!
-	// 끝냈다면, 에디터 세팅에서 Player의 Collision Preset에서 새 Coliision Channel, Item을 만들고, Ovelap 만 활성화!
-	// 이 액터의 콜리전 채널을 Item으로 하고, Player의 것도 켜 주세요!
-	AMRPlayerCharacter* PlayerCharacter = Cast<AMRPlayerCharacter>(OtherActor);
-	if (PlayerCharacter && ItemType != EItemEffectTypes::None)
-	{
-		if (PickupSound)
-		{
-			UGameplayStatics::PlaySoundAtLocation(this, PickupSoundCue, GetActorLocation());
-		}
-
-		if (PickupEffect)
-		{
-			UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), PickupEffect, GetActorLocation());
-		}
-		/*UE_LOG(LogTemp, Warning, TEXT("충돌: %s(%s) 와 %s(%s)"),
-			*GetName(), *OverlappedComp->GetName(),
-			*OtherActor->GetName(), *OtherComp->GetName());*/
-		PlayerCharacter->ItemActivated(ItemType);
-
-		SetActorHiddenInGame(true);
-		SetActorEnableCollision(false);
-
-		FTimerHandle TimerHandle;
-		GetWorldTimerManager().SetTimer(TimerHandle, this, &AItemBaseActor::DestroySelf, 0.8f, false);
-	}
-}
-
-void AItemBaseActor::DestroySelf()
-{
-	Destroy();
 }
 
--- Source/MuscleRun/Object/Item/ItemBaseActor.h
+++ Source/MuscleRun/Object/Item/ItemBaseActor.h
@@ -27,8 +27,6 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	virtual void Destroyed() override;
-
 private:
 	UPROPERTY(VisibleAnywhere, Category = "Item")
 	class UBoxComponent* TriggerVolume;
@@ -60,4 +58,6 @@ protected:
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	void DestroySelf();
+	void PlayPickupEffect();
+	void PlayVanishEffect();
 };
